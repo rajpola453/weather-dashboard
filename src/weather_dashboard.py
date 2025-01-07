18db71aa0d130e57461bbc539b7770dc375b75ba
rajpola453 <rajpola453@gmail.com>
2025-01-06 23:23:01 -0600

    Project-1 completed

--- src/weather_dashboard.py
+++ src/weather_dashboard.py
@@ -73,7 +73,7 @@ def main():
     # Create bucket if needed
     dashboard.create_bucket_if_not_exists()
     
-    cities = ["Philadelphia", "Seattle", "New York"]
+    cities = ["Philadelphia", "Seattle", "New York", "Illinois"]
     
     for city in cities:
         print(f"\nFetching weather for {city}...")
